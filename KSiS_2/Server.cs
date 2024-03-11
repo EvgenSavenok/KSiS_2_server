540fe16507df3fdb2fc73644f65aef676fa8aebe
EvgenSavenok <eugen.savenok@gmail.com>
2024-03-11 15:01:09 +0300

    Chat works

--- KSiS_2/Server.cs
+++ KSiS_2/Server.cs
@@ -3,10 +3,12 @@ using System.Net.Sockets;
 using System.Text;
 class Server
 {
-    private int nextClientID = 0;
+    int nextClientID = 0;
+    string prevMessage = null;
+    bool isLocalChatStarted = false, isFirstMessage = true;
     Dictionary<int, Socket> clientsSockets = new Dictionary<int, Socket>();
     Dictionary<int, string> clientsNames = new Dictionary<int, string>();
-    Dictionary<int, int> localModeClients = new Dictionary<int, int>();
+    Dictionary<int, int> localChats = new Dictionary<int, int>();
     private IPEndPoint? GetData()
     {
         int port;
@@ -50,35 +52,142 @@ class Server
         }
         return listener;
     }
-    void SendToAllClients(byte[] message, int excludingClientID = -1)
+    private void DisconnectClient(Socket clientSocket, int clientID, string clientName)
     {
-        foreach (var pair in clientsSockets)
+        clientSocket.Shutdown(SocketShutdown.Both);
+        clientSocket.Close();
+        clientsSockets.Remove(clientID);
+        clientsNames.Remove(clientID);
+        Console.WriteLine($"Пользователь с ID {clientID} и именем {clientName} покинул чат.");
+    }
+    private void OutputTableOfUsers(string clientName, int clientID)
+    {
+        StringBuilder sb = new StringBuilder();
+        Console.WriteLine($"Пользователь {clientName} с ID {clientID} запросил список всех пользователей.");
+        sb.AppendLine("Список всех пользователей:");
+        foreach (var line in clientsNames)
+            sb.AppendLine($"Имя: {line.Value}, ID: {line.Key}");
+        byte[] byteMessage = Encoding.UTF8.GetBytes(sb.ToString());
+        clientsSockets[clientID].Send(byteMessage);
+    }
+    private void SendToClient(byte[] message, int clientID)
+    {
+        if (clientsSockets.ContainsKey(clientID))
         {
-            if (pair.Key != excludingClientID)
-                pair.Value.Send(message);
+            clientsSockets[clientID].Send(message);
+        }
+    }
+    private void StartLocalChatWithClient(string receiveMessage, Socket clientSocket, int clientID)
+    {
+        byte[] byteMessage;
+        if (isFirstMessage)
+        {
+            byteMessage = Encoding.UTF8.GetBytes($"Вы вошли в чат с {clientsNames[Int32.Parse(receiveMessage)]}");
+            SendToClient(byteMessage, clientID);
+            byteMessage = Encoding.UTF8.GetBytes($"Пользователь {clientsNames[clientID]}" +
+                $" пригласил Вас в чат.\nЕсли Вы хотите выйти из этого чата, наберите /exit.");
+            SendToClient(byteMessage, localChats[clientID]);
+            isFirstMessage = false;
+            localChats.Add(Int32.Parse(receiveMessage), clientID);
+        }
+        if (receiveMessage.ToLower() == "/exit")
+        {
+            string exitModeMessage = $"Вы вышли из чата с {clientsNames[localChats[clientID]]}";
+            byte[] exitModeMsg = Encoding.UTF8.GetBytes(exitModeMessage);
+            SendToClient(exitModeMsg, clientID);
+            isLocalChatStarted = false;
+            isFirstMessage = true;
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.Append($"{clientsNames[clientID]}: ");
+        sb.Append(receiveMessage);
+        byteMessage = Encoding.UTF8.GetBytes(sb.ToString());
+        SendToClient(byteMessage, localChats[clientID]);
+    }
+    private void CheckMessage(string receivedMessage, Socket clientSocket, int clientID, string clientName)
+    {
+        if (receivedMessage == "/1")
+        {
+            OutputTableOfUsers(clientName, clientID);
+            prevMessage = receivedMessage;
+        }
+        else if (receivedMessage == "/2")
+        {
+            DisconnectClient(clientSocket, clientID, clientName);
+            return;
+        }
+        if ((prevMessage == "/1") && (receivedMessage != prevMessage))
+        {
+            try
+            {
+                if (clientsNames.ContainsKey(Int32.Parse(receivedMessage)) && (clientID != Int32.Parse(receivedMessage)))
+                {
+                    isLocalChatStarted = true;
+                    localChats.Add(clientID, Int32.Parse(receivedMessage)); 
+                    //currentLocalClientID = Int32.Parse(receivedMessage);
+               ///////////////////////////////////////////////////////////////
+                }
+                else
+                {
+                    string errorMessage = $"Вы не можете написать самому себе " +
+                        $"или пользователя с таким ID не существует.";
+                    byte[] errorBytes = Encoding.UTF8.GetBytes(errorMessage);
+                    SendToClient(errorBytes, clientID);
+                }
+            }
+            catch (Exception) 
+            {
+                Console.WriteLine($"Клиент c ID {clientID} ввел некорректный ID.");
+            }
+        }
+        if (isLocalChatStarted)
+        {
+            prevMessage = null;
+            StartLocalChatWithClient(receivedMessage, clientSocket, clientID);
+        }
+    }
+    private void HandleClient(Socket clientSocket, int clientID, string clientName)
+    {
+        try
+        {
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+                int numOfBytes = clientSocket.Receive(buffer);
+                if (numOfBytes == 0)
+                {
+                    DisconnectClient(clientSocket, clientID, clientName);
+                    break;
+                }
+                string receivedMessage = Encoding.UTF8.GetString(buffer, 0, numOfBytes);
+                Console.WriteLine($"Получено от клиента с ID {clientID}: {receivedMessage}");
+                CheckMessage(receivedMessage, clientSocket, clientID, clientName);
+            }
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine(e.ToString());
         }
     }
     private void StartListening(Socket listener)
     {
         while (true) 
         {
-            Socket handler = listener.Accept();
+            Socket clientSocket = listener.Accept();
             int clientID = nextClientID++;
-            clientsSockets.Add(clientID, handler);
+            clientsSockets.Add(clientID, clientSocket);
             byte[] nameBuffer = new byte[1024];
-            int nameBytesRec = handler.Receive(nameBuffer);
+            int nameBytesRec = clientSocket.Receive(nameBuffer);
             string clientName = Encoding.UTF8.GetString(nameBuffer, 0, nameBytesRec);
             clientsNames.Add(clientID, clientName);
-            string connectMessage = $"Клиент {clientName} с id {clientID} подключился к чату";
+            string connectMessage = $"Клиент {clientName} с ID {clientID} подключился к чату";
             Console.WriteLine(connectMessage);
-            string message = $"Пользователь {clientName} вошел в чат";
-            byte[] msg = Encoding.UTF8.GetBytes(message);
-            SendToAllClients(msg, clientID);
+            Task.Run(() => HandleClient(clientSocket, clientID, clientName));
         }
     }
     private void TryToStartListening(IPEndPoint localEndPoint, Socket listener)
     {
-        const int NUM_OF_BACKLOGS = 5;
+        const int NUM_OF_BACKLOGS = 20;
         try 
         {
             listener.Bind(localEndPoint);
